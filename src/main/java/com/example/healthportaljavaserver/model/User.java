964a8509ff0933de5810d2f9e68cdb11e81110a3
Celia Sherry <sherry.c@husky.neu.edu>
2019-03-23 16:25:40 -0400

    craeted database connection

---
+++ src/main/java/com/example/healthportaljavaserver/model/User.java
@@ -0,0 +1,7 @@
+package com.example.healthportaljavaserver.model;
+
+import javax.persistence.Entity;
+
+public class User {
+
+}
---
+++ src/main/java/com/example/healthportaljavaserver/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.healthportaljavaserver.repositories;
+
+public class UserRepository {
+
+}
